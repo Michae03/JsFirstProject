b6244a7628029c2127d1a8f1afb4530b02cce7f6
Michal Nowak <michal.now03.gmail.com>
2023-05-26 00:35:04 +0200

    fix/ code improvment

---
+++ Scripts/questions.js
@@ -0,0 +1,307 @@
+questions = [{
+    content:  "Ile to 2+2?",
+    category: "math",
+    answers: [{
+      content: "2", 
+      right: false
+    }, {
+      content: "3",
+      right: false
+    }, {
+      content: "4",
+      right: true
+    }, {
+      content: "5",
+      right: false
+    }
+  ]}, {
+    content:  "'Ile boków ma kwadrat?",
+    category: "math",
+    answers: [{
+      content: "Cztery", 
+      right: true
+    }, {
+      content: "Dwa",
+      right: false
+    }, {
+      content: "Trzy",
+      right: false
+    }, {
+      content: "Pięć",
+      right: false
+    }
+  ]}, {
+    content:  "Kto był pierwszym zabójcą w Dead By Daylight?",
+    category: "dead by daylight",
+    answers: [{
+      content: "Trapper", 
+      right: true
+    }, {
+      content: "Huntress",
+      right: false
+    }, {
+      content: "Wraith",
+      right: false
+    }, {
+      content: "Nurse",
+      right: false
+    }
+  ]}, {
+    content:  "Jakie zwierze robi muuu",
+    category: "biology",
+    answers: [{
+      content: "Krowa", 
+      right: true
+    }, {
+      content: "Żaba",
+      right: false
+    }, {
+      content: "Komar",
+      right: false
+    }, {
+      content: "Zuzia",
+      right: false
+    }
+  ]}, {
+    content:  "Której z wymienionych postaci nie ma w lidze legend:",
+    category: "league of legends",
+    answers: [{
+      content: "Albert Wesker", 
+      right: true
+    }, {
+      content: "Ahri",
+      right: false
+    }, {
+      content: "Tryndamere",
+      right: false
+    }, {
+      content: "Yuumi",
+      right: false
+    }
+  ]}, {
+    content:  "W którym roku wyszło Dead by Daylight?",
+    category: "dead by daylight",
+    answers: [{
+      content: "2015", 
+      right: false
+    }, {
+      content: "2016",
+      right: true
+    }, {
+      content: "2018",
+      right: false
+    }, {
+      content: "2017",
+      right: false
+    }
+  ]}, {
+    content:  "Od jakiego surivora jest perk Balanced Landing?",
+    category: "dead by daylight",
+    answers: [{
+      content: "Adam Francis", 
+      right: false
+    }, {
+      content: "Meg Thomas",
+      right: false
+    }, {
+      content: "Nea Karlsson",
+      right: true
+    }, {
+      content: "David King",
+      right: false
+    }
+  ]}, {
+    content:  "Od jakiego survivora jest perk Lithe?",
+    category: "dead by daylight",
+    answers: [{
+      content: "Feng Min", 
+      right: true
+    }, {
+      content: "Claudette Morel",
+      right: false
+    }, {
+      content: "Mikaela Reid",
+      right: false
+    }, {
+      content: "Dwight Farfield",
+      right: false
+    }
+  ]}, {
+    content:  "Jakie jest prawdziwe imię Otzdarvy?",
+    category: "dead by daylight",
+    answers: [{
+      content: "John", 
+      right: false
+    }, {
+      content: "Alex",
+      right: true
+    }, {
+      content: "Max",
+      right: false
+    }, {
+      content: "Michael",
+      right: false
+    }
+  ]}, {
+    content:  "Wraz z jakim zabójcą wyszedł dodatek z Yui Kimurą?",
+    category: "dead by daylight",
+    answers: [{
+      content: "Clown", 
+      right: false
+    }, {
+      content: "Oni",
+      right: true
+    }, {
+      content: "The Nightmare",
+      right: false
+    }, {
+      content: "Michael Myers",
+      right: false
+    }
+  ]}, {
+    content:  "Czyim bratem jest Yone?",
+    category: "league of legends",
+    answers: [{
+      content: "Lee Sin", 
+      right: false
+    }, {
+      content: "Shen",
+      right: false
+    }, {
+      content: "Yasuo",
+      right: true
+    }, {
+      content: "Viego",
+      right: false
+    }
+  ]}, {
+    content:  "Kto został wtrącony do więzienia?",
+    category: "league of legends",
+    answers: [{
+      content: "Annie", 
+      right: false
+    }, {
+      content: "Sylas",
+      right: true
+    }, {
+      content: "Akali",
+      right: false
+    }, {
+      content: "Maokai",
+      right: false
+    }
+  ]}, {
+    content:  "Jak ma na imię miś Annie?",
+    category: "league of legends",
+    answers: [{
+      content: "Yummi", 
+      right: false
+    }, {
+      content: "Max",
+      right: false
+    }, {
+      content: "Tibbers",
+      right: true
+    }, {
+      content: "Minnie",
+      right: false
+    }
+  ]}, {
+    content:  "Jak ma na imię dziadek Jotaro?",
+    category: "anime",
+    answers: [{
+      content: "Jonathan", 
+      right: false
+    }, {
+      content: "Joseph",
+      right: true
+    }, {
+      content: "Joe",
+      right: false
+    }, {
+      content: "Iggy",
+      right: false
+    }
+  ]}, {
+    content:  "Jak ma na imię przywódca grupy gangsterskiej z 5 partu?",
+    category: "anime",
+    answers: [{
+      content: "Giorno Giovanna", 
+      right: false
+    }, {
+      content: "Guido Mista",
+      right: false
+    }, {
+      content: "Bruno Buciaratti",
+      right: true
+    }, {
+      content: "Leone Abbacchoio",
+      right: false
+    }
+  ]}, {
+    content:  "Jak nazywa się najlepszy przyjaciel Gona Freecss'a?",
+    category: "anime",
+    answers: [{
+      content: "Hisoka", 
+      right: false
+    }, {
+      content: "Killua",
+      right: true
+    }, {
+      content: "Illumi",
+      right: false
+    }, {
+      content: "Feitan",
+      right: false
+    }
+    ]}, {
+    content:  "Jak ma na imię brat Edwarda Elrica?",
+    category: "anime",
+    answers: [{
+      content: "Alphonse", 
+      right: true
+    }, {
+      content: "Roy",
+      right: false
+    }, {
+      content: "Envy",
+      right: false
+    }, {
+      content: "Hughes",
+      right: false
+    }
+  ]}, {
+    content:  "Pod jakim pseudonimem znany był Walter White?",
+    category: "tv series",
+    answers: [{
+      content: "Tuco", 
+      right: false
+    }, {
+      content: "Heisenberg",
+      right: true
+    }, {
+      content: "Willy Wonka",
+      right: false
+    }, {
+      content: "Pinkman",
+      right: false
+    }
+  ]}, {
+    content:  "Jakiego przedmiotu uczył Walter White?",
+    category: "tv series",
+    answers: [{
+      content: "Matematyki", 
+      right: false
+    }, {
+      content: "Fizyki",
+      right: false
+    }, {
+      content: "Chemii",
+      right: true
+    }, {
+      content: "Informatyki",
+      right: false
+    }
+  ]}
+];
+
--- Scripts/quiz-time-quiz.js
+++ Scripts/quiz-time-quiz.js
@@ -1,142 +1,101 @@
-questions = [
-  createQuestion('Ile to 2+2?','4','3','2','5'),
-  createQuestion('Jakie jest moje ulubione piwo?', 'Żubr', 'Jack', 'Sommersby', 'Fortuna'),
-  createQuestion('Ile boków ma kwadrat?', 'Cztery', 'Dwa', 'Trzy', 'Pięć'),
-  createQuestion('Kto był pierwszym zabójcą w Dead By Daylight?', 'Trapper', 'Huntress', 'Wraith', 'Nurse'),
-  createQuestion('Jakie zwierze robi muuuu', 'Krowa', 'Żaba', 'Komar', 'Zuzia'),
-  createQuestion('Której z wymienionych postaci nie ma w lidze legend:', 'Albert Wesker', 'Ahri', 'Tryndamere', 'Yuumi')
-]
 let actualQuestion;
-let questionCounter = 0;
-const timer = document.querySelector('.js-timer');
+let questionCounter=0;
+const timer=document.querySelector('.js-timer');
 let intervalId;
 nextQuestion();
-initButtons();
 
 //Defined Functions
 
-function initButtons() {
-  const redButton = document.querySelector('.js-button1');
-  const blueButton = document.querySelector('.js-button2');
-  const yellowButton = document.querySelector('.js-button3');
-  const greenButton = document.querySelector('.js-button4');
-
-  redButton.addEventListener('click', () =>{
-    buttonClick(redButton.innerHTML)
-  });
-
-  blueButton.addEventListener('click', () =>{
-    buttonClick(blueButton.innerHTML)
-  });
-
-  yellowButton.addEventListener('click', () =>{
-    buttonClick(yellowButton.innerHTML)
-  });
-
-  greenButton.addEventListener('click', () =>{
-    buttonClick(greenButton.innerHTML)
-  });
-}
-
-function buttonClick(buttonHTML) {
-  if(checkIfRightAnswer(buttonHTML)) {
-    addPoint();
-    nextQuestion();
-  } else {
-    substractPoint();
-    nextQuestion();
-  }
-}
-
-function createQuestion(questionContent, rightAnswer, wrongA1, wrongA2, wrongA3) {
-  const question = {
-    content: questionContent,
-    firstAnswer: {
-      content: rightAnswer,
-      right: true
-    },
-    secondAnswer: {
-      content: wrongA1,
-      right: false
-    },
-    thirdAnswer: {
-      content: wrongA2,
-      right: false
-    },
-    fourthAnswer: {
-      content: wrongA3,
-      right: false
-    }
-  };
-  return question
-}
-
-function pickRandomQuestion(questions) {
-  shuffle(questions);
-  return questions[0]
-}
-
-function displayQuestion(question) {
-  const QELEMENT = document.querySelector('.js-question-content');
-  QELEMENT.innerHTML = question.content;
-
-  const firstAnswer = question.firstAnswer.content;
-  const secondAnswer = question.secondAnswer.content;
-  const thirdAnswer = question.thirdAnswer.content;
-  const fourthAnswer = question.fourthAnswer.content;
-  answers = [firstAnswer, secondAnswer, thirdAnswer, fourthAnswer];
-
-  shuffle(answers);
-
-  for(let i=4; i > 0; i--) {
-    const button = document.querySelector(`.js-button${i}`);
-    button.innerHTML = answers[i-1];
-  }
-
-  document.querySelector('.js-question-title').innerHTML = `Pytanie ${questionCounter}`;
-}
-
-function checkIfRightAnswer(buttonInnerHtml) {
-  let rightAnswer = actualQuestion.firstAnswer.content;
-  if(buttonInnerHtml === rightAnswer){
-    return true;
-  } else {
-    return false;
-  }
+function updateButtonClickData() {
+  document.querySelectorAll(".js-answer-button").forEach((button) => {
+    button.addEventListener('click', () => {
+      //Checks if the answer is right
+      const {dataset: {answerIsRight}}=button;
+      if(answerIsRight) {
+        addPoint();
+        nextQuestion();
+      } else {
+        substractPoint();
+        nextQuestion();
+      }
+    })
+  })
 }
 
 function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
+  let currentIndex=array.length,  randomIndex;
 
   // While there remain elements to shuffle.
   while (currentIndex != 0) {
 
     // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
+    randomIndex=Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
+    [array[currentIndex], array[randomIndex]]=[
       array[randomIndex], array[currentIndex]];
   }
 
   return array;
 }
 
+function pickRandomQuestion(questions) {
+  shuffle(questions);
+  console.log(questions);
+  return questions[0] ? questions.shift() : {
+    content:  "ERROR: no more questions",
+    category: "math",
+    answers: [{
+      content: "true", 
+      right: true
+    }, {
+      content: "true",
+      right: true
+    }, {
+      content: "false",
+      right: false
+    }, {
+      content: "false",
+      right: false
+    }
+  ]}
+}
+
+function displayQuestion(question) {
+  shuffle(question.answers);
+  
+  let questioncontainerHTML=`
+    <div class="question-title js-question-title">Pytanie ${questionCounter}</div>
+    <div class="question-content js-question-content">${question.content}</div>
+  `
+  let answercontainerHTML=`
+    <button class="red-button js-answer-button" data-answer-is-right="${question.answers[0].right ? 'true' : ''}">${question.answers[0].content}</button>
+    <button class="blue-button js-answer-button" data-answer-is-right="${question.answers[1].right ? 'true' : ''}">${question.answers[1].content}</button>
+    <button class="yellow-button js-answer-button" data-answer-is-right="${question.answers[2].right ? 'true' : ''}">${question.answers[2].content}</button>
+    <button class="green-button js-answer-button" data-answer-is-right="${question.answers[3].right ? 'true' : ''}">${question.answers[3].content}</button>
+  `;
+
+  document.querySelector(".js-answer-container").innerHTML=answercontainerHTML;
+  document.querySelector(".js-question-container").innerHTML=questioncontainerHTML;
+  
+}
+
 function nextQuestion() {
-  actualQuestion = pickRandomQuestion(questions);
+  actualQuestion=pickRandomQuestion(questions);
   questionCounter ++;
   displayQuestion(actualQuestion);
+  updateButtonClickData();
   updateTimer();
 }
 
 function updateTimer() {
   clearInterval(intervalId);
-  time = 10;
-  timer.innerHTML = `${time}s`
-  intervalId = setInterval(() => {
+  time=10;
+  timer.innerHTML=`${time}s`
+  intervalId=setInterval(() => {
     time --;
-    timer.innerHTML = `${time}s`;
+    timer.innerHTML=`${time}s`;
     if(time < 1) {
       outOfTime();
     }    
@@ -144,13 +103,13 @@ function updateTimer() {
 }
 
 function addPoint() {
-  counter = document.querySelector('.js-correct-answer-count');
-  counter.innerHTML = `${(Number(counter.innerHTML) + 1)}`
+  counter=document.querySelector('.js-correct-answer-count');
+  counter.innerHTML=`${(Number(counter.innerHTML) + 1)}`
 }
 
 function substractPoint() {
-  counter = document.querySelector('.js-incorrect-answer-count');
-  counter.innerHTML = `${(Number(counter.innerHTML) + 1)}`
+  counter=document.querySelector('.js-incorrect-answer-count');
+  counter.innerHTML=`${(Number(counter.innerHTML) + 1)}`
 }
 
 function outOfTime() {
